3a46f3b71c6013bade8457918094730437f7c705
dudipingu <oliver.dudler@bluewin.ch>
2018-03-23 14:54:14 +0100

    Started with the UI and all that...

---
+++ DriveAppCloudModelTest/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  DriveAppCloudModelTest
+//
+//  Created by oliver dudler on 23.03.18.
+//  Copyright © 2018 Oliver-Dudler. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var personNameLabel: UILabel!
+    @IBOutlet weak var personHairColorLabel: UILabel!
+    @IBOutlet weak var petLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+}
--- DriveAppCloudModelTest/ViewController.swift
+++ DriveAppCloudModelTest/ViewController.swift
@@ -9,18 +9,59 @@
 import UIKit
 import CloudKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var persons = ["Frieda", "Peter"]
+    var hairColor = ["blonde", "brown"]
+    var petRace = ["cat", "dog"]
+    var petAge = [1,2]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        
+        cell.personNameLabel.text = persons[indexPath.row]
+        cell.personHairColorLabel.text = hairColor[indexPath.row]
+        
+        return cell
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 2
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 150
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
 
