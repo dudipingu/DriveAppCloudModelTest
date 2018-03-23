aa367b97b50d55635ebe53af37da48c2d600876c
dudipingu <oliver.dudler@bluewin.ch>
2018-03-23 13:40:03 +0100

    Initial commit 2

---
+++ DriveAppCloudModelTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DriveAppCloudModelTest
+//
+//  Created by oliver dudler on 23.03.18.
+//  Copyright © 2018 Oliver-Dudler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
